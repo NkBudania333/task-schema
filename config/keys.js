1d2fd1f044f34a2a53d7a801b84f23fe7f5bff3b
NkBudania <2025niteshkumar2001@gmail.com>
2021-03-23 02:22:02 +0530

    my first commit

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+dbPassword =
+  // "mongodb+srv://Aditi:VbefhCni04u06609@data-cluster.2keyl.mongodb.net/test?retryWrites=true&w=majority";
+
+ "mongodb+srv://nk:NKBUDANIA@cluster0.03czq.mongodb.net/Internshala_node?retryWrites=true&w=majority";
+module.exports = {
+  mongoURI: dbPassword,
+};
---
+++ routes/index.js
@@ -0,0 +1,48 @@
+const experss = require("express");
+const router = experss.Router();
+const passport = require("passport");
+const { ensureAuthenticated } = require("../config/auth");
+const User = require("../models/Users");
+const flash = require("connect-flash");
+
+router.get("/", (req, res) => res.render("login"));
+
+router.get("/dashboard", ensureAuthenticated, (req, res) => {
+    res.render("dashboard", {
+        name: req.user.name,
+    });
+});
+
+router.get("/tasks/:name", (req, res) => {
+    res.render("dashboard", {
+        name: req.params.name
+    })
+})
+
+router.post("/tasks/:name", (req, res, next) => {
+    // console.log(req.body.toString());
+    const obj = JSON.parse(JSON.stringify(req.body));
+
+    if(obj.newtask == ""){
+        res.render("dashboard", {
+            name: req.params.name,
+        })
+    }
+    else{
+        User.findOneAndUpdate(
+            {name: req.params.name},
+            { $addToSet: { tasks: obj.newtask }},
+            { upsert: true, new: true },
+            function(err, value) {
+                res.render("dashboard", {
+                    name: req.params.name,
+                    value: value.tasks,
+                })
+                console.log(value.tasks);.03
+                265
+            }
+        )
+    }
+})
+
+module.exports = router;
